8b2f61b14a1ed27b2b0ff7242f4325b473312408
Anthony Tu <antu3199@gmail.com>
2020-08-15 21:25:14 -0400

    Basic game loop working

--- MizJam/Assets/_Scripts/BasicMap.cs
+++ MizJam/Assets/_Scripts/BasicMap.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -23,19 +24,45 @@ public class BasicMap : MonoBehaviour
     public bool moveMap = true;
     public float moveMapSpeed = 1f;
 
-
+    protected Action<BasicMap> onLoadNextMap = null;
+    protected Action<BasicMap> onDestroy = null;
+    protected Action<BasicMap> onSetAsCenterMap = null;
     private bool onInitializeCalled = false;
     
 
-    public void Initialize() {
+    public void Initialize(Action<BasicMap> onLoadNextMap, Action<BasicMap> onDestroy, Action<BasicMap> setAsCenterMap, Vector3 referencePosition) {
+        this.transform.position = referencePosition + new Vector3(VISIBLE_X, 0, 0);
+        this.CommonInitialization(onLoadNextMap, onDestroy, setAsCenterMap);
+    }
+    public void Initialize(Action<BasicMap> onLoadNextMap, Action<BasicMap> onDestroy, Action<BasicMap> setAsCenterMap) {
+        this.CommonInitialization(onLoadNextMap, onDestroy, setAsCenterMap);
+    }
+
+    private void CommonInitialization(Action<BasicMap> onLoadNextMap, Action<BasicMap> onDestroy, Action<BasicMap> setAsCenterMap) {
+        this.onLoadNextMap = onLoadNextMap;
+        this.onDestroy = onDestroy;
+        this.onSetAsCenterMap = setAsCenterMap;
+
         MapEvent destroyEvent = new MapEvent();
+        destroyEvent.identifier = "Destroy event";
         destroyEvent.positionToTrigger = -VISIBLE_X;
         destroyEvent.triggerEvent = DestroyMap;
         this.mapEvents.Add(destroyEvent);
 
+        MapEvent loadNextMapEvent = new MapEvent();
+        loadNextMapEvent.identifier = "Load next map event";
+        loadNextMapEvent.positionToTrigger = VISIBLE_X/2;
+        loadNextMapEvent.triggerEvent = this.LoadNextMap;
+        this.mapEvents.Add(loadNextMapEvent);
+
+        MapEvent setCenterMapEvent = new MapEvent();
+        setCenterMapEvent.identifier = "Load next map event";
+        setCenterMapEvent.positionToTrigger = VISIBLE_X/2;
+        setCenterMapEvent.triggerEvent = this.LoadNextMap;
+        this.mapEvents.Add(loadNextMapEvent);
+
         this.DoInitialize();
     }
-
 
     // Update is called once per frame
     public void PerformUpdate()
@@ -45,9 +72,10 @@ public class BasicMap : MonoBehaviour
             MapEvent mapEvent = mapEvents[i];
             if (!mapEvent.hasTriggered && this.GetPosition() <= mapEvent.positionToTrigger) {
                 if (mapEvent.triggerEvent != null) {
+                    Debug.Log(this.name + " Trigger event: " + mapEvent.identifier);
                     mapEvent.triggerEvent();
                 } else {
-                    Debug.LogError("ERROR: Trigger event was null!");
+                    Debug.LogError("ERROR: Trigger event was null!: " + mapEvent.identifier);
                 }
 
                 mapEvent.hasTriggered = true;
@@ -66,11 +94,10 @@ public class BasicMap : MonoBehaviour
     }
     
     public virtual void DoInitialize() {
-
     }
 
     public virtual void DestroyMap() {
-        Debug.Log("Destroy Map: " + this.gameObject.name);
+        this.onDestroy(this);
         Destroy(this.gameObject);
     }
 
@@ -84,6 +111,14 @@ public class BasicMap : MonoBehaviour
 
     protected float GetPosition() {
         return this.transform.position.x;
+    }
+
+    protected void LoadNextMap() {
+        this.onLoadNextMap(this);
+    }
+
+    protected void SetAsCenterMap() {
+        this.onSetAsCenterMap(this);
     }
 
 }
--- MizJam/Assets/_Scripts/MapScroller.cs
+++ MizJam/Assets/_Scripts/MapScroller.cs
@@ -1,22 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 
 
 public class MapScroller : MonoBehaviour
 {
 
-    public BasicMap currentMap;
+    public List<BasicMap> possibleMaps;
+
+    public BasicMap currentMap; // Set this to starting map at the start
+
+    public Grid grid; // Used for parent
+
     public float mapMoveSpeed = 1f;
+    public List<BasicMap> activeMaps{get; set;}
 
     public void Initialize() {
-        this.currentMap.Initialize();
+        this.activeMaps = new List<BasicMap>();
+        this.activeMaps.Add(this.currentMap);
+        this.currentMap.Initialize(this.OnLoadNextMap, this.OnDestroyMap, this.SetAsCenterMap );
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.currentMap.PerformUpdate();
+        for (int i = 0; i < this.activeMaps.Count; i++) {
+            BasicMap map = this.activeMaps[i];
+            map.PerformUpdate();
+        }
+    }
+
+    private void OnDestroyMap(BasicMap map) {
+        this.activeMaps.Remove(map);
+    }
+
+    private void OnLoadNextMap(BasicMap map) {
+        int randMapIndex = UnityEngine.Random.Range(0, possibleMaps.Count);
+        // randMapIndex = 1;
+        BasicMap newMap = Instantiate(possibleMaps[randMapIndex], grid.transform) as BasicMap;
+        newMap.Initialize(this.OnLoadNextMap, this.OnDestroyMap, this.SetAsCenterMap, map.transform.position);
+        this.activeMaps.Add(newMap);
+    }
+
+    private void SetAsCenterMap(BasicMap map) {
+        this.currentMap = map;
     }
 }
