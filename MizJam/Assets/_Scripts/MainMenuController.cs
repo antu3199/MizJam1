697bfe5e7c0b6bada82eece6995194edf58b57cd
Anthony Tu <antu3199@gmail.com>
2020-08-22 16:02:24 -0400

    maybe done?

---
+++ MizJam/Assets/_Scripts/MainMenuController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Text playButtonText;
+    void Start()
+    {
+        this.playButtonText.text = GameManager.Instance.gameState.hasPlayedTutorial ? "Continue" : "Play";
+    }
+}
